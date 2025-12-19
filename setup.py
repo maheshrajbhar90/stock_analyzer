01c455a3f4be769e7843097b87960ee5ba806cf5
Mahesh Kumar <127003692+maheshrajbhar90@users.noreply.github.com>
2025-12-19 13:15:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stock_analyzer',
-    version='0.1.0',
+    version='0.0.0.1',
     author='Mahesh Kumar',
     author_email='maheshrajbhar90@gmail.com',
 
@@ -27,3 +27,4 @@ setup(
     ],
     python_requires='>=3.8',
 )
+
