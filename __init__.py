ce8ab4e8e64b7a4bf39cddc5d6429efecb40bd45
Mahesh Kumar <127003692+maheshrajbhar90@users.noreply.github.com>
2025-12-19 13:12:03 +0530

    Initialize stock_analyzer package with metadata
    Added package metadata and imports for stock_analyzer.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+AutoSmartAPI Package
+This package contains modules for extracting data and genrating the signal, and creating prompt for LLM models.
+
+Author: Mahesh Kumar
+Email: maheshrajbhar90@gmail.com
+Version: 0.0.0.1
+"""
+
+# Import the main classes and functions that should be accessible when the package is imported
+from .stock_analyzer import FundamentalAnalyser  # Assuming your main class is in a file named 'stock_analyzer.py'
+
+# You can also define version number here if needed
+__version__ = '0.0.0.1'
+
+
