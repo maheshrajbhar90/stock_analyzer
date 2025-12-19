1700ed30b09d3d600102ed0be6aace93992f48b0
Mahesh Kumar <127003692+maheshrajbhar90@users.noreply.github.com>
2025-12-19 13:28:09 +0530

    Import FundamentalAnalyser in package init

--- stock_analyzer/__init__.py
+++ stock_analyzer/__init__.py
@@ -10,6 +10,8 @@ Version: 0.0.0.1
 
 # Import the main classes and functions that should be accessible when the package is imported
 from .stock_analyzer import StockTechnicalAnalyzer  # Assuming your main class is in a file named 'stock_analyzer.py'
+from .fundamental_analyzer import FundamentalAnalyser
+
 
 # You can also define version number here if needed
 __version__ = '0.0.0.1'
